e0217de69cfd23c024ca27956bc24527191dadf6
Nacho Valera Herrera <nachovaleraherreraes@gmail.com>
2016-02-01 21:50:43 +0100

    Now the phone on dummies is randomly created

--- src/application/modules/users/model/kernel/Dummies.java
+++ src/application/modules/users/model/kernel/Dummies.java
@@ -1,7 +1,6 @@
 package application.modules.users.model.kernel;
 
 import javax.swing.JOptionPane;
-
 import application.models.SingletonF;
 import application.modules.users.model.BLL.Arraylist_admin;
 import application.modules.users.model.models.admin;
@@ -20,9 +19,10 @@ public class Dummies {
 		int a = Functions.validateint("How many dummies do you want to add?", "Add Dummies");
 		for(int i=0; i<a;i++){
 			int random=(int)(Math.random()*100000000);
+			int phone=(int)(Math.random()*1000000000);
 			e=letraDNI(random);
 			System.out.print(lettercalc);
-			admin dummy1 = new admin(e, "dummy", "dummy", "987654321", "asdf@asdf.asdf", "Usuario", "Pass",
+			admin dummy1 = new admin(e, "dummy", "dummy", Integer.toString(phone), "asdf@asdf.asdf", "Usuario", "Pass",
 					"Avatar", "Online", "10/10/1990", "14/12/2014",1234.1234f, 1234);
 			singleton.admins.addData(dummy1);
 		}
